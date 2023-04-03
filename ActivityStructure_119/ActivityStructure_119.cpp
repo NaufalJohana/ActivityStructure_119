e9d0b1632d1a90dbea7f784b69358add84266a76
NaufalJohana <naufaljohana@gmail.com>
2023-04-03 10:48:07 +0700

    memasukan data mahasiswa 1

--- ActivityStructure_119/ActivityStructure_119.cpp
+++ ActivityStructure_119/ActivityStructure_119.cpp
@@ -16,7 +16,7 @@ int main() {
 	mhs1.NIM = "20220140119";
 	mhs1.nama = "ananda naufal";
 	mhs1.alamat = "Purworejo";
-	mhs1.umur = 18;
+	mhs1.umur = 21;
 
 	cout << "Masukan NIM :";
 	cin >> mhs2.NIM;
@@ -26,6 +26,16 @@ int main() {
 	cin >> mhs2.alamat;
 	cout << "Masukan umur :";
 	cin >> mhs2.umur;
+
+	cout << "\nNIM = " << mhs1.NIM;
+	cout << "\nNama = " << mhs1.nama;
+	cout << "\nAlamat = " << mhs1.alamat;
+	cout << "\nUmur = " << mhs1.umur;
+
+	cout << "\n\nNim = " << mhs2.NIM;
+	cout << "\nNama = " << mhs2.nama;
+	cout << "\nAlamat = " << mhs2.alamat;
+	cout << "\nUmur = " << mhs2.umur;
 
 }
 
